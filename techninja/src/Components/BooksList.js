a24973b069b108f3672c967a186902729c4cd825
= <=>
2022-09-05 15:39:42 +0300

    Added Navbar and BookList components

---
+++ techninja/src/Components/BooksList.js
@@ -0,0 +1,15 @@
+import React from 'react'
+
+function BooksList() {
+  return (
+    <div className='book-list'>
+        <ul>
+            <li>Game of Numbers</li>
+            <li>Garden City</li>
+            <li>The Last Pirate</li>
+        </ul>
+    </div>
+  )
+}
+
+export default BooksList
